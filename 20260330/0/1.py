7997aa7e18a71979a40760629b89b4422569bc9b
Dmitrii Gudov <gudovdo@my.msu.ru>
2026-03-30 14:19:51 +0300

    work work

---
+++ 20260330/0/1.py
@@ -0,0 +1,18 @@
+"""Merge parent config into the child options.
+
+The migration process requires an `options` object for the child in
+order to distinguish between mutually exclusive codes, add-select and
+add-ignore error codes.
+
+"""
+
+from math import *
+
+sin(123)
+
+line = "    PID TTY          TIME CMD             9100 pts/1    00:00:03 zsh 9143 136:1    00:00:00 zsh 9194 136:1    00:00:00 zsh 9195 136:1    00:00:00 zsh"
+
+
+def foo():
+    """foooo."""
+    pass
